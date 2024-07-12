8ae279c7051a877f14adc05a0e8933327594ac70
rishi2025 <jharishi2025@gmail.com>
2024-07-12 23:19:37 +0530

    configuring redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Footer, Header } from "./components/index.jsx";
+// import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  console.log(import.meta.env.VITE_TEST);
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+
+        if (userData)
+          dispatch(login({ userData }));
+
+        else
+          dispatch(logout());
+
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <>
-      <h1> Hello World </h1>
-    </>
+    loading ?
+      (
+        <h1>Loading...</h1>
+      ):
+      (
+        <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+          <div className="w-full block">
+            <Header />
+            <main>
+              {
+                // <Outlet />
+              }
+            </main>
+            <Footer />
+          </div>
+        </div>
+    )
   )
 }
 
-export default App
+export default App;
