a65cdcee2a84f5d5cf0be1d017dd5394f161a290
rishi2025 <jharishi2025@gmail.com>
2024-07-13 18:26:46 +0530

    Components design implemented

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,23 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Login from "./Login";
+import Button from './Button';
+import Input from './Input';
+import Select from "./Select";
+import PostCard from "./PostCard";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Login,
+    Button,
+    Input,
+    Select,
+    PostCard,
 }
